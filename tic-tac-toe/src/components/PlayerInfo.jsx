db8a823db5e74011065ba0cb3696c0f5e938f63c
Desmond <desmondmpofu@gmail.com>
2024-05-01 00:40:18 +0200

    dynamic renders

--- tic-tac-toe/src/components/PlayerInfo.jsx
+++ tic-tac-toe/src/components/PlayerInfo.jsx
@@ -1,11 +1,25 @@
+import React, {useState} from 'react'
 export default function PlayerInfo({player,symbol}) {
+   const [isEditing, setIsEditing] = useState(false)
+
+    const handleClick = () => {
+        setIsEditing(prev=>!prev)
+    }
+
+    let playerName = <span className="player-name">{player}</span>
+    let btnCaption = 'Edit'
+    if (isEditing){
+        playerName=<input type='text' required/>
+        btnCaption='Save'
+    }
+
     return(
         <li>
         <span className="player">
-        <span className="player-name">{player}</span>
+        {playerName}
         <span className="player-symbol">{symbol}</span>
         </span>
-        <button>Edit</button>
+        <button onClick={handleClick}>{btnCaption}</button>
     </li>
     )
 }
