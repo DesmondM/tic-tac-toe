09d6b7915f1e3a71b8665be24ce476c1a9cade99
Desmond <desmondmpofu@gmail.com>
2024-05-02 11:11:34 +0200

    switching players

--- tic-tac-toe/src/App.jsx
+++ tic-tac-toe/src/App.jsx
@@ -1,18 +1,24 @@
+import React, {useState} from "react"
 import Gameboard from "./components/Gameboard"
 import PlayerInfo from "./components/PlayerInfo"
 
 function App() {
+
+    const [activePlayer, setActivePlayer]= useState('X')
   
+    function handleSelectSquare(){
+        setActivePlayer((activePlayer)=>activePlayer==='X'?'O':'X')
+    }
 
   return (
     <main>
         <div id='game-container'>
-            <ol id='players'>
-               <PlayerInfo initialName="Player 1" symbol="X"/>
-               <PlayerInfo initialName="Player 2" symbol="O"/>
+            <ol id='players' className='highlight-player'>
+               <PlayerInfo initialName="Player 1" symbol="X" isActive={activePlayer==='X'}/>
+               <PlayerInfo initialName="Player 2" symbol="O" isActive={activePlayer==='X'}/>
 
             </ol>
-            <Gameboard/>
+            <Gameboard onSelectSquare={handleSelectSquare} activePlayerSymbol={activePlayer}/>
         </div>
     </main>
   )
--- tic-tac-toe/src/components/PlayerInfo.jsx
+++ tic-tac-toe/src/components/PlayerInfo.jsx
@@ -1,5 +1,5 @@
 import React, {useState} from 'react'
-export default function PlayerInfo({initialName, symbol}) {
+export default function PlayerInfo({initialName, symbol, isActive}) {
    const [playerName, setPlayerName] = useState(initialName)
    const [isEditing, setIsEditing] = useState(false)
 
@@ -17,7 +17,7 @@ export default function PlayerInfo({initialName, symbol}) {
     }
 
     return(
-        <li>
+        <li className={isActive?'active':undefined}>
         <span className="player">
         {editablePlayerName}
         <span className="player-symbol">{symbol}</span>
